4faae979ec27fa79e80c6cba7d9a9604b2ec9658
Leonardo <leobusta@gmail.com>
2019-09-03 20:10:10 -0500

    Intro vue

---
+++ src/components/Intro.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      {{ mensaje }}
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['mensaje'],
+    data()  {
+        return {       
+            mess: this.mensaje
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
